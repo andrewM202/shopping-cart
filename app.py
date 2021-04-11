c405fdd0efee58fb7a6aaa5cb28e68732bfcbc92
Andrew Matt <andrewm1246@gmail.com>
2021-04-11 13:05:02 -0400

    use database_url variable than its actual variable

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_migrate import Migrate
 import psycopg2
 
-ON_HEROKU = os.environ.get('ON_HEROKU')
-
-if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    port = 3000
-
-# creating the connection to the postgreSQL database
-
 app = Flask(__name__)
 # Figure out what this below does:
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -26,7 +16,8 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 from sqlalchemy import create_engine
-db_string = "postgres://lefkbggqqoctza:f3efc8f36726f9fefd8cecd5bfb549e94162c08ca91326a330b065b018ebe6cc@ec2-34-225-103-117.compute-1.amazonaws.com:5432/d5jr1dkvvnk67r"
+db_string = DATABASE_URL
+#db_string = "postgres://lefkbggqqoctza:f3efc8f36726f9fefd8cecd5bfb549e94162c08ca91326a330b065b018ebe6cc@ec2-34-225-103-117.compute-1.amazonaws.com:5432/d5jr1dkvvnk67r"
 #"postgresql://andrewmatt:@localhost/shopping_cart"
 db = create_engine(db_string)
 
